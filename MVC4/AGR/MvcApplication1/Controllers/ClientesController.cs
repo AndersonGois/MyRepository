7b1429e14d05b8d4d6f731255e6c2d4067e8a0ec
andersonGois <andersonrejane@hotmail.com>
2012-09-23 21:22:59 -0300

    Viiew Clientes

---
+++ MVC4/AGR/MvcApplication1/Controllers/ClientesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ClientesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
